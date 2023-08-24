3e336cedc99301fada4ebb59ec34724ae9e9a478
T-Gamer <pedromarcallima@gmail.com>
2023-08-24 17:13:16 -0300

    Primeiros Exercicios;

---
+++ Unidade01/src/SomarDoisNumeros.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class SomarDoisNumeros {
+   public static Scanner sc = new Scanner(System.in);
+   
+   public static void main(String args[])
+   {
+	   int num1, num2;
+	  System.out.println("Digite um numero");
+	   num1 = sc.nextInt();
+	   System.out.println("Digite outro numero");
+	   num2= sc.nextInt();
+	   int Soma = num1+num2;
+	   System.out.println("SOMA:"+Soma);
+	   
+   }
+}
